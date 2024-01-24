aeb74ceb9c48b44dfbc066a849a34be4d73128e8
Test1 1 <williamh2461@gmail.com>
2024-01-24 09:25:41 -0500

    Made some changes

--- src/Services.js
+++ src/Services.js
@@ -58,20 +58,20 @@ const Services = () => {
     <div className="row">
       <div className="col-lg-4">
       <img src={cheryl} alt='logo' className='' width="140" height="140" />
-        <h2>Kitchens</h2>
+        <h2>Move in/Move outs</h2>
         <p>All sinks, surfaces, and floors, includes inside microwaves, cabinets and outside refrigerator.</p>
     
       </div>    
       <div className="col-lg-4">
       <img src={lisa} alt='logo' className='' width="140" height="140" />
-        <h2>Bathrooms</h2>
+        <h2>Garbage and oganization</h2>
         <p>All sinks, surfaces, toilets, showers, mirrors and floors. Including cleaning tiles and disinfecting towels.</p>
        
       </div>
       <div className="col-lg-4">
         {/* <svg className="bd-placeholder-img rounded-circle " width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
         <img src={amy} alt='logo' className='' width="140" height="140" />
-        <h2>Whole House</h2>
+        <h2>One Time Options</h2>
         <p>We take out garbage and tidy all rooms, we also dust and vacuum.</p>
        
       </div>
